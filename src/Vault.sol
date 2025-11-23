bb7e0e7ecc25518f57c6ac2faa457973291d9341
Chris Smith <1408372+iamchrissmith@users.noreply.github.com>
2025-11-23 16:33:38 -0700

    fix storage layout

--- src/Vault.sol
+++ src/Vault.sol
@@ -37,12 +37,14 @@ contract Vault is IVault, ERC4626 {
     uint256       public immutable LP_DUST;
 
     // STATE VARIABLES
-    uint256[]                   private buckets;
+    uint256[] private buckets;
+
+    uint8                       public bolt;                  // reentrancy lock: 0 = off, 1 = on
     mapping(uint256 => uint256) public bucketsIndex;          // (bucketIndex => index location in buckets)
     uint256                     public bufferLps;
     mapping(uint256 => uint256) public lps;                   // (bucketIndex => lps)
     uint256                     public removedCollateralValue;
-    uint8                       public bolt;                  // reentrancy lock: 0 = off, 1 = on
+
 
     // MODIFIERS
     modifier lock() {
